a0d9304fb30ca1cfb6aef567e9dc5dd0c7c767e6
rancococ <rancococ@qq.com>
2017-11-21 23:11:37 +0800

    banner 佛主保佑

---
+++ spring-boot-demo-service/spring-boot-demo-service-calc/src/main/java/com/catvgd/springbootdemo/common/tkmapper/mapper/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.catvgd.springbootdemo.common.tkmapper.mapper;
+
+import com.catvgd.springbootdemo.common.tkmapper.model.SysUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SysUserMapper extends Mapper<SysUser> {
+}
--- spring-boot-demo/spring-boot-demo-service/spring-boot-demo-service-calc/src/main/java/com/catvgd/springbootdemo/common/tkmapper/mapper/SysAccountMapper.java
+++
@@ -1,7 +0,0 @@
-package com.catvgd.springbootdemo.common.tkmapper.mapper;
-
-import com.catvgd.springbootdemo.common.tkmapper.model.SysAccount;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SysAccountMapper extends Mapper<SysAccount> {
-}

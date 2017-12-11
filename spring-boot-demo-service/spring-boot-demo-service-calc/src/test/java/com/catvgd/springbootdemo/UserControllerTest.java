57ea9f392b1f04f3d9bb3cb3557cd1b24283eabc
rancococ <rancococ@qq.com>
2017-12-11 22:39:41 +0800

    更新mvc的测试类

--- spring-boot-demo-service/spring-boot-demo-service-calc/src/test/java/com/catvgd/springbootdemo/UserControllerTest.java
+++ spring-boot-demo-service/spring-boot-demo-service-calc/src/test/java/com/catvgd/springbootdemo/UserControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,22 +12,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import com.catvgd.springbootdemo.biz.calc.controller.UserController;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EnableAutoConfiguration
 public class UserControllerTest {
 
     @Autowired
-    private UserController controller;
-
+    private WebApplicationContext context;
     private MockMvc mvc;
 
     @Before
     public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     @Test
